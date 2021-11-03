f399362ca03d68f03603ef8b9fb10cb9eb35663d
GeneKong <gyx_edu@qq.com>
2021-11-03 18:43:50 +0800

    调整日志等级

--- src/BluFi.cpp
+++ src/BluFi.cpp
@@ -571,8 +571,8 @@ void BluFi::eventHandler(esp_blufi_cb_event_t event, esp_blufi_cb_param_t *param
     }
 #endif
     case ESP_BLUFI_EVENT_RECV_CUSTOM_DATA:
-        log_i("Recv Custom Data %d", param->custom_data.data_len);
-        esp_log_buffer_hex("Custom Data", param->custom_data.data, param->custom_data.data_len);
+        log_d("Recv Custom Data %d", param->custom_data.data_len);
+        //esp_log_buffer_hex("Custom Data", param->custom_data.data, param->custom_data.data_len);
         if (_custom_data_recv_cb != NULL)
             _custom_data_recv_cb(param->custom_data.data, param->custom_data.data_len);
         break;
